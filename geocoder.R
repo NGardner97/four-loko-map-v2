c5236a0c8671cb4dae09ee1eeb303974c1c4bb8d
NGardner97 <nicholas.gardner97@gmail.com>
2025-06-26 22:37:44 +0100

    added push pull function

--- geocoder.R
+++ geocoder.R
@@ -1,6 +1,8 @@
 library(tidygeocoder)
 library(tidyverse)
 
+system("git pull origin main")
+
 df <- read_csv("four-loko-sightings-raw.csv")
 
 df <- df %>%
@@ -10,3 +12,7 @@ df_geo <- df %>%
   geocode(address = full_address, method = "osm", lat = latitude, long = longitude)
 
 write_csv(df_geo, "four-loko-sightings.csv")
+
+system("git add four-loko-sightings.csv")
+system("git commit -m 'Automated update from R'")
+system("git push origin main")
