94834344587f2ca398804b02af87894e0c1be4be
NGardner97 <nicholas.gardner97@gmail.com>
2025-08-30 13:48:17 +0100

    update to only geocode new entries

--- geocoder.R
+++ geocoder.R
@@ -3,13 +3,18 @@ library(tidyverse)
 
 system("git pull origin main")
 
-df <- read_csv("four-loko-sightings-raw.csv")
+df <- read_csv("four-loko-sightings.csv")
 
 df <- df %>%
   mutate(full_address = paste(address, area, postcode, "UK", sep = ", "))
 
-df_geo <- df %>%
+df_done <- df %>% filter(!is.na(latitude) & !is.na(longitude))
+df_todo <- df %>% filter(is.na(latitude) | is.na(longitude))
+
+df_todo_geo <- df_todo %>%
   geocode(address = full_address, method = "osm", lat = latitude, long = longitude)
+
+df_geo <- bind_rows(df_done, df_todo_geo)
 
 write_csv(df_geo, "four-loko-sightings.csv")
 
